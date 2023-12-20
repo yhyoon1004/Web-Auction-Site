cf76e73c1d64914cb03018f77335cb9f875fdcd0
yunhwan <yhyoon1004@naver.com>
2023-12-20 22:37:57 +0900

    Initial commit

---
+++ src/main/java/com/example/auction/config/WebSocketConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.auction.config;
+
+import org.springframework.boot.autoconfigure.graphql.reactive.GraphQlWebFluxAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        
+    }
+}
